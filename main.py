d58c9ae2d61ec56806a7b7abbe1de70b27c368b2
Дима <mtt1@list.ru>
2023-12-09 19:11:17 +0300

    переработана фунция получения погоды

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ from datetime import datetime
 
 import requests
 from dotenv import load_dotenv
-from translate import Translator
+
 
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN_API')
-
-translator = Translator(from_lang="English", to_lang="russian")
 
 DICT_ICON_WEATHER = {
     "Clear": "Ясно \U00002680",
@@ -23,18 +21,19 @@ DICT_ICON_WEATHER = {
 }
 
 
-async def get_weather(city: str, key=TOKEN):
-
+# async def get_weather(city: str, key=TOKEN):
+def get_weather(city: str, key: str):
     try:
-        response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=metric")
+
+        response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&lang=ru&appid={key}&units=metric")
         result = response.json()
-        name_city = translator.translate(result['name'])
+        # pprint(result)
         weather_description = DICT_ICON_WEATHER.get(result['weather'][0]['main'],
                                                     "Посмотри в окно, не пойму что там за погода")
         sunrise = datetime.fromtimestamp(result['sys']['sunrise'])
         sunset = datetime.fromtimestamp(result['sys']['sunset'])
         return f"Дата {datetime.fromtimestamp(result['dt']).strftime('%d.%m.%Y')} \n" \
-               f"Город: {name_city} \n" \
+               f"Город: {result['name']} \n" \
                f"Температура воздуха: {result['main']['temp']} C {weather_description}\n" \
                f"Влажность: {result['main']['humidity']} % \n" \
                f"Давление: {result['main']['pressure']} мм.рт.ст. \n" \
@@ -44,3 +43,7 @@ async def get_weather(city: str, key=TOKEN):
                f"Продолжительность дня: {sunset - sunrise}"
     except:
         return "\U00002620 Проверь название города \U00002620"
+
+
+if __name__ == "__main__":
+    print(get_weather("сочи", key=TOKEN))
