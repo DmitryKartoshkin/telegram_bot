dcbb60534a8d86517f03ed09876ae237c52186fd
Дима <mtt1@list.ru>
2023-12-17 12:54:11 +0300

    добавлены клавиатура

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -4,13 +4,28 @@ from aiogram.types import (
     InlineKeyboardMarkup,
     InlineKeyboardButton
 )
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+from config.config import DICT_CURRENCY
 
 main_keybord = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="/weather")]
+        [
+            KeyboardButton(text="/weather"),
+            KeyboardButton(text="/currency_convector"),
+         ]
     ],
     resize_keybord=True,
     one_time_keybord=True,
     input_field_placeholder="Команды запуска",
     selective=True
 )
+
+
+def currency_keybord():
+    builder = ReplyKeyboardBuilder()
+    for i in [*DICT_CURRENCY.values()]:
+        builder.add(KeyboardButton(text=str(i)))
+    return builder.adjust(4)
+
+
