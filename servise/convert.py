d3413fe936819bcc715b18431113a398b657f43a
Дима <mtt1@list.ru>
2023-12-17 12:51:26 +0300

    перенесены константы в config.py

--- servise/convert.py
+++ servise/convert.py
@@ -1,17 +1,8 @@
 from pprint import pprint
-# import os
 
 import requests
 
 from config.config import YOUR_API_KEY
-
-
-# from dotenv import load_dotenv
-
-# load_dotenv()
-# YOUR_API_KEY = os.getenv('YOUR_API_KEY')
-
-# YOUR_API_KEY = "fe403357e740d588cde6a26f"
 
 
 def conv():
@@ -21,26 +12,16 @@ def conv():
 
 
 # conv()
-DICT_CURRENCY = {'AED': "Дирхам ОАЭ", 'ARS': "Аргентинское пе́со", 'AUD': "Австралийский доллар",
-                 'AMD': "Армянский драм", 'AZN': " Азербайджанский манат", 'BGN': "Болгарский лев",
-                 'BRL': "Бразильский реал", 'BYN': "Белорусский рубль", 'CAD': "Канадский доллар",
-                 'CHF': "Швейцарский франк", 'CNY': "Китайский юань", 'COP': "Колумбийское песо",
-                 'CUP': "Кубинское песо", 'CZK': "Чешская крона", 'DKK': "Датская крона",
-                 'EGP': "Египетский фунт", 'EUR': "евро", 'GBP': "фунт стерлинг", 'GEL': "Грузинский лари",
-                 'HKD': "Гонконгский доллар", 'HUF': "Венгерский форинт", 'ILS': "шекель", 'INR': "Индийская рупия",
-                 'JPY': "иена", 'KGS': "Киргизский сом", 'KRW': "Южнокорейская вона", 'KZT': "Казахстанский тенге",
-                 'MDL': "Молдавский лей ", 'MXN': "Мексиканское песо", 'NOK': "Норвежская крона",
-                 'PLN': "Польский злотый", 'RON': "Румынский лей", 'RSD': "Сербский динар", 'RUB': "рубль",
-                 'SAR': "Саудовский риял", 'SEK': "шведская крона", 'SGD': "Сингапурский доллар",
-                 'THB': "Таиландский бат", 'TJS': "Таджикский сомони", 'TMT': "Новый туркменский манат",
-                 'TRY': "Турецкая лира", 'UAH': "Украинская гривна", 'USD': "доллар", 'UZS': "Узбекский су",
-                 'VND': "Вьетнамский донг"}
 
-LIST_COUNTRY = []
 
-def conversion(currency: str):
+def conversion(currency: str, TOKEN=YOUR_API_KEY):
 
-    url = f'https://v6.exchangerate-api.com/v6/{YOUR_API_KEY}/latest/USD'
+    url = f'https://v6.exchangerate-api.com/v6/{TOKEN}/latest/{currency}'
     response = requests.get(url)
     data = response.json()
-    pprint(data)
+    pprint(data['conversion_rates'])
+
+
+def currency_convector():
+    pass
+conversion("EUR")
