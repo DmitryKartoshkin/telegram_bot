63254526321187b767e4ff7d8fefdf138ce82702
Дима <mtt1@list.ru>
2023-12-28 17:39:56 +0300

    убран лишний код

--- app/tg_bot.py
+++ app/tg_bot.py
@@ -65,7 +65,6 @@ async def currency(message: types.Message, state: FSMContext):
     await message.answer(cur)
 
 
-
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
