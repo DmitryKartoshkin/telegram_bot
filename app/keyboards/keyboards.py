342d618a981abe0f4f9028fca2839a5f8449bfed
Дима <mtt1@list.ru>
2023-12-30 15:26:34 +0300

    изменена клавиатура

---
+++ app/keyboards/keyboards.py
@@ -0,0 +1,23 @@
+from aiogram.types import ReplyKeyboardMarkup
+from aiogram.types import KeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+from app.config.config import DICT_CURRENCY_2
+
+
+def get_start_kb() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardBuilder()
+    kb.button(text="/weather")
+    kb.button(text="/currency_convector")
+    kb.adjust(2)
+    return kb.as_markup(resize_keyboard=True)
+
+
+def currency_keyboard() -> ReplyKeyboardMarkup:
+    builder = ReplyKeyboardBuilder()
+    for i in [*DICT_CURRENCY_2.keys()]:
+        builder.add(KeyboardButton(text=str(i)))
+    builder.adjust(4)
+    return builder.as_markup(resize_keyboard=True)
+
+
