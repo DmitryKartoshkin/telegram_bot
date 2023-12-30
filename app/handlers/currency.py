4662e55d8aa6a2858857d18fba3da29d48148dba
Дима <mtt1@list.ru>
2023-12-30 15:26:16 +0300

    изменена структура проекта

---
+++ app/handlers/currency.py
@@ -0,0 +1,37 @@
+from aiogram.fsm.state import StatesGroup, State
+from aiogram import Router, F
+from aiogram.filters import Command
+from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.fsm.context import FSMContext
+
+from app.keyboards.keyboards import currency_keyboard
+from app.servise.convert import currency_convector
+
+router = Router()
+
+
+class CurrencyForm(StatesGroup):
+    currency = State()
+    count = State()
+
+
+@router.message(Command("currency_convector"))
+async def cmd_weather(message: Message, state: FSMContext):
+    await state.set_state(CurrencyForm.currency)
+    await message.answer("Выберете валюту", reply_markup=currency_keyboard())
+
+
+@router.message(CurrencyForm.currency)
+async def currency(message: Message, state: FSMContext):
+    await state.update_data(currency=message.text)
+    await state.set_state(CurrencyForm.count)
+    await message.answer("Выберете количество для расчета")
+
+
+@router.message(CurrencyForm.count)
+async def currency(message: Message, state: FSMContext):
+    await state.update_data(count=message.text)
+    data = await state.get_data()
+    await state.clear()
+    cur = await currency_convector(data["currency"], int(data["count"]))
+    await message.answer(cur)
