e9ce9cdba9d95fdd13b71da57afe978d234a4122
Дима <mtt1@list.ru>
2023-12-30 15:29:37 +0300

    изменена стркуктура в соответствии с роутерами

--- app/tg_bot.py
+++ app/tg_bot.py
@@ -1,75 +1,25 @@
 import asyncio
-from aiogram import Bot, types, Dispatcher
-from aiogram.filters import Command
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import StatesGroup, State
+from aiogram import Bot, Dispatcher
 
-from config.config import BOT_TOKEN
-from keyboards.keyboards import main_keyboard, currency_keyboard
-from servise.convert import currency_convector
-from servise.weather import get_weather
+from handlers import questions, weather, filter_words
+from app.config.config import BOT_TOKEN
 
 
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-
-
-class WeatherForm(StatesGroup):
-    city = State()
-
-
-class CurrencyForm(StatesGroup):
-    currency = State()
-    count = State()
-
-
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Hello!", reply_markup=main_keyboard)
-
-
-@dp.message(Command("weather"))
-async def cmd_weather(message: types.Message, state: FSMContext):
-    await state.set_state(WeatherForm.city)
-    await message.answer("Введите город")
-
-
-@dp.message(WeatherForm.city)
-async def form_city(message: types.Message, state: FSMContext):
-    await state.update_data(city=message.text)
-    data = await state.get_data()
-    await state.clear()
-    weather_forecast = await get_weather(data["city"])
-    await message.answer(weather_forecast)
-
-
-@dp.message(Command("currency_convector"))
-async def cmd_weather(message: types.Message, state: FSMContext):
-    await state.set_state(CurrencyForm.currency)
-    await message.answer("Выберете валюту", reply_markup=currency_keyboard().as_markup(resize_keyboard=True))
-
-
-@dp.message(CurrencyForm.currency)
-async def currency(message: types.Message, state: FSMContext):
-    await state.update_data(currency=message.text)
-    await state.set_state(CurrencyForm.count)
-    await message.answer("Выберете количество для расчета")
-
-
-@dp.message(CurrencyForm.count)
-async def currency(message: types.Message, state: FSMContext):
-    await state.update_data(count=message.text)
-    data = await state.get_data()
-    await state.clear()
-    cur = await currency_convector(data["currency"], int(data["count"]))
-    await message.answer(cur)
-
-
+# Запуск бота
 async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
+    # Альтернативный вариант регистрации роутеров по одному на строку
+    dp.include_router(questions.router)
+    dp.include_router(weather.router)
+    dp.include_router(filter_words.router)
+
+    # Запускаем бота и пропускаем все накопленные входящие
+    # Да, этот метод можно вызвать даже если у вас поллинг
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
+
 if __name__ == "__main__":
     asyncio.run(main())
-
-
