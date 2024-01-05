6e7da67ccf894cc4988647215f29591013c724be
Дима <mtt1@list.ru>
2024-01-05 16:08:43 +0300

    изменен импорт

--- app/servise/convert.py
+++ app/servise/convert.py
@@ -2,10 +2,10 @@ from datetime import datetime
 
 import requests
 
-from app.config.config import YOUR_API_KEY, DICT_CURRENCY_1, DICT_CURRENCY_2
+from config.config import config
 
 
-async def _conversion(currency: str, TOKEN=YOUR_API_KEY):
+async def _conversion(currency: str, TOKEN=config.YOUR_API_KEY):
     url = f'https://v6.exchangerate-api.com/v6/{TOKEN}/latest/{currency}'
     response = requests.get(url)
     data = response.json()
@@ -13,12 +13,12 @@ async def _conversion(currency: str, TOKEN=YOUR_API_KEY):
 
 
 async def currency_convector(curr: str, numb: int):
-    currency = DICT_CURRENCY_2.get(curr)
+    currency = config.DICT_CURRENCY_2.get(curr)
     res = await _conversion(currency)
     return f"Курс валюты на {datetime.today().strftime('%d-%m-%Y')} \n" \
-           f"{numb} {DICT_CURRENCY_1.get(currency)}: {numb * res['USD']} {DICT_CURRENCY_1.get('USD')} \n" \
-           f"{numb} {DICT_CURRENCY_1.get(currency)}: {numb * res['EUR']} {DICT_CURRENCY_1.get('EUR')} \n" \
-           f"{numb} {DICT_CURRENCY_1.get(currency)}: {numb * res['RUB']} {DICT_CURRENCY_1.get('RUB')}"
+           f"{numb} {config.DICT_CURRENCY_1.get(currency)}: {numb * res['USD']} {config.DICT_CURRENCY_1.get('USD')}\n" \
+           f"{numb} {config.DICT_CURRENCY_1.get(currency)}: {numb * res['EUR']} {config.DICT_CURRENCY_1.get('EUR')}\n" \
+           f"{numb} {config.DICT_CURRENCY_1.get(currency)}: {numb * res['RUB']} {config.DICT_CURRENCY_1.get('RUB')}"
 
 
 
